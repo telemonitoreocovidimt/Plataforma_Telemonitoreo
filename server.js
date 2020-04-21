6bb58fad850c371d6e7a49054141b47719e4e288
Victor López <vic@Victors-MacBook-Pro.local>
2020-04-20 21:24:35 -0500

    test upload excel

--- server.js
+++ server.js
@@ -3,6 +3,18 @@ const path = require("path")
 const app = express()
 const session = require('express-session')
 const { KEY_SECRET, PORT } = require("./config")
+const formData = require("express-form-data");
+const os = require("os");
+
+const options = {
+  uploadDir: os.tmpdir(),
+  autoClean: true
+};
+
+app.use(formData.parse(options));
+app.use(formData.format());
+app.use(formData.stream());
+app.use(formData.union());
 
 app.use(express.urlencoded({extended:true}))
 
@@ -20,5 +32,7 @@ app.use(session({
 app.use(require("./router/account/index"))
 
 app.use("/dashboard", require("./router/dashboard/index"))
+
+app.use("/load-excel", require("./router/loadExcel/index"))
 
 app.listen( 3000 || PORT)
